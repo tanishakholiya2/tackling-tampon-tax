bb936d34eee28357d3a251122f09ff98bc2445bf
tanishakholiya2 <tanishakholiya@gmail.com>
2022-08-13 23:00:51 -0700

    database

--- loginstuff.js
+++ loginstuff.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-apiKey: "AIzaSyD9A24ZeypiKY7fGqaA0sMCO9sGEhJZYro",
-authDomain: "ttt3-6fa02.firebaseapp.com",
-projectId: "ttt3-6fa02",
-storageBucket: "ttt3-6fa02.appspot.com",
-messagingSenderId: "763195936058",
-appId: "1:763195936058:web:5f17a66d44891caff8d403",
-measurementId: "G-R56VCP0D38"
-};
+    apiKey: "AIzaSyD9A24ZeypiKY7fGqaA0sMCO9sGEhJZYro",
+    authDomain: "ttt3-6fa02.firebaseapp.com",
+    databaseURL: "https://ttt3-6fa02-default-rtdb.firebaseio.com",
+    projectId: "ttt3-6fa02",
+    storageBucket: "ttt3-6fa02.appspot.com",
+    messagingSenderId: "763195936058",
+    appId: "1:763195936058:web:5f17a66d44891caff8d403",
+    measurementId: "G-R56VCP0D38"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,6 +15,7 @@ const auth = firebase.auth();
 firebase.auth().onAuthStateChanged(function(user) {
 if (user) {
     // User is signed in.
+    console.log(user.email);
     document.getElementById('user').innerHTML = user.email;
     document.getElementById('sign-out').style.visibility = 'visible';
     let signOutButton = document.getElementById("sign-out");
